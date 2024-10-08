18beacdabdded3aff4905e47e4d52b580a8706e7
Izaiah <iilozano@ucsc.edu>
2024-10-08 13:37:24 -0700

    First Step Complete - Added button & title change

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,29 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName =
-  "My totally awesome spectacular rambuctious incredible mind blowing game";
+  "Goodest Boy Ever";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+
+
+function createButton() {
+
+  const btn = document.createElement('button');
+  btn.textContent = '🐶';
+
+  btn.style.position = 'absolute';
+  btn.style.bottom = "160px";
+  btn.style.left = "600px";
+
+  btn.addEventListener('click', () => {
+    alert('I woof you!');
+  });
+
+  document.body.appendChild(btn);
+}
+
+document.addEventListener('DOMContentLoaded', createButton);

d67b3f6c05039405b7a00e1fdb14ba0d2f7bdcda
Izaiah <iilozano@ucsc.edu>
2024-10-08 15:03:03 -0700

    Step 3 Complete - Counter now has Auto-increments

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ app.append(header);
 let head_pat: number = 0;
 
 function createButton() {
-  
   //Button Settings
   const btn = document.createElement("button");
   btn.textContent = "🐶";
@@ -21,9 +20,9 @@ function createButton() {
   btn.style.left = "600px";
 
   //Counter Settings
-  const how_many_pats = document.createElement('div');
-  how_many_pats.style.marginTop = '20px';
-  how_many_pats.style.fontSize = '18px';
+  const how_many_pats = document.createElement("div");
+  how_many_pats.style.marginTop = "20px";
+  how_many_pats.style.fontSize = "18px";
   how_many_pats.textContent = `${head_pat} head pats for the goodest boy ever`;
   how_many_pats.style.position = "absolute";
   how_many_pats.style.bottom = "130px";
@@ -35,7 +34,7 @@ function createButton() {
   });
 
   document.body.appendChild(btn);
-  document.body.appendChild(how_many_pats)
+  document.body.appendChild(how_many_pats);
 }
 
 document.addEventListener("DOMContentLoaded", createButton);

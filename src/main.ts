2865dd4a001a3acb7dc0e3d984090b6c8eb8e092
Izaiah <iilozano@ucsc.edu>
2024-10-08 14:17:09 -0700

    Step 2 Complete - Implemented a counter for clicks

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,40 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName =
-  "Goodest Boy Ever";
+const gameName = "Goodest Boy Ever";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-
+let head_pat: number = 0;
 
 function createButton() {
-
-  const btn = document.createElement('button');
-  btn.textContent = '🐶';
-
-  btn.style.position = 'absolute';
+  
+  //Button Settings
+  const btn = document.createElement("button");
+  btn.textContent = "🐶";
+  btn.style.position = "absolute";
   btn.style.bottom = "160px";
   btn.style.left = "600px";
 
-  btn.addEventListener('click', () => {
-    alert('I woof you!');
+  //Counter Settings
+  const how_many_pats = document.createElement('div');
+  how_many_pats.style.marginTop = '20px';
+  how_many_pats.style.fontSize = '18px';
+  how_many_pats.textContent = `${head_pat} head pats for the goodest boy ever`;
+  how_many_pats.style.position = "absolute";
+  how_many_pats.style.bottom = "130px";
+  how_many_pats.style.left = "475px";
+
+  btn.addEventListener("click", () => {
+    head_pat++;
+    how_many_pats.textContent = `${head_pat} head pats for the goodest boy ever`;
   });
 
   document.body.appendChild(btn);
+  document.body.appendChild(how_many_pats)
 }
 
-document.addEventListener('DOMContentLoaded', createButton);
+document.addEventListener("DOMContentLoaded", createButton);

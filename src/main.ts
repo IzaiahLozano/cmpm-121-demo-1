4151bf2cc4097c1d29e8ceb4b04e056db49e7846
Izaiah <iilozano@ucsc.edu>
2024-10-09 13:39:16 -0700

    Step 4 Complete: Switched to Framerate increments + Fixed alignment for any sized screen

--- src/main.ts
+++ src/main.ts
@@ -10,31 +10,59 @@ header.innerHTML = gameName;
 app.append(header);
 
 let head_pat: number = 0;
+const how_many_pats = document.createElement("div");
+const btn = document.createElement("button");
 
-function createButton() {
+function Game_setup() {
   //Button Settings
-  const btn = document.createElement("button");
   btn.textContent = "🐶";
-  btn.style.position = "absolute";
-  btn.style.bottom = "160px";
-  btn.style.left = "600px";
 
   //Counter Settings
-  const how_many_pats = document.createElement("div");
   how_many_pats.style.marginTop = "20px";
   how_many_pats.style.fontSize = "18px";
-  how_many_pats.textContent = `${head_pat} head pats for the goodest boy ever`;
-  how_many_pats.style.position = "absolute";
-  how_many_pats.style.bottom = "130px";
-  how_many_pats.style.left = "475px";
-
-  btn.addEventListener("click", () => {
-    head_pat++;
-    how_many_pats.textContent = `${head_pat} head pats for the goodest boy ever`;
-  });
+  how_many_pats.textContent = `${head_pat} head pats for the goodest boy ever`; //Inital value -> 0
 
   document.body.appendChild(btn);
   document.body.appendChild(how_many_pats);
 }
 
-document.addEventListener("DOMContentLoaded", createButton);
+
+
+function Count_Behavior() {
+  
+  let start = 0;
+
+  function updateCounter(timestamp: number) {
+
+    if (start === undefined) {
+      start = timestamp;
+    } 
+
+    const time_passed = timestamp - start;
+    const increase = time_passed/1000;
+
+    //console.log(time_passed);
+    //console.log(increase); #Ensuring proper behavior 
+
+    head_pat += increase; 
+    
+    how_many_pats.textContent = `${Math.floor(head_pat)} head pats for the goodest boy ever`;
+
+    start = timestamp;
+
+    requestAnimationFrame(updateCounter);
+  }
+
+
+  btn.addEventListener("click", () => {
+    head_pat++;
+    how_many_pats.textContent = `${Math.floor(head_pat)} head pats for the goodest boy ever`;
+  });
+
+  //Frame increments
+  requestAnimationFrame(updateCounter);
+}
+
+
+document.addEventListener("DOMContentLoaded", Game_setup);
+document.addEventListener("DOMContentLoaded", Count_Behavior);
